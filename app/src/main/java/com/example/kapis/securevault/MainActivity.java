62e0ee66bbbca7ac1f580daf4c6021b6237d10df
Ahmad M <moosem1234@gmail.com>
2018-11-03 18:31:11 -0400

    Adding ability to take a picture with camera

--- app/src/main/java/com/example/kapis/securevault/MainActivity.java
+++ app/src/main/java/com/example/kapis/securevault/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.kapis.securevault;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +15,14 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -25,12 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
     String[] mClasses;
     List<String> lstClasses;
-    TextView mTv;
+
     LinearLayout mLinearLayout;
-    Button mBtn;
     ListView mLv;
     MyAdapter adapter;
     String str="";
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    String mCurrentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
         lstClasses=new ArrayList<String>(Arrays.asList(mClasses));
 
-        mTv=(TextView)findViewById(R.id.main_Header);
         mLinearLayout=(LinearLayout)findViewById(R.id.main_Linear);
 
-        mBtn = new Button(this);
         mLv=new ListView(this);
 
         adapter=new MyAdapter(this,-1,mClasses);
@@ -92,6 +102,56 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
 
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this,"Error: Photo not saved. Try again.",Toast.LENGTH_SHORT).show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+
+    //In here we will allow the user to add a new folder
+    @OnClick(R.id.mainNewFolder)
+    public void addNewFolder() {
+
+        dispatchTakePictureIntent();
+
+    }
+
+
 
 
     public void ChristmasParty() {
